caf21ca388fb80669ee0d4e07316ee5da398b87e
SPallai <sangeeta.sumair@gmail.com>
2019-01-14 19:01:44 -0800

    updated files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Big Mac", false),
+  ("Whooper", false),
+  ("Fish", false),
+  ("Salmon", false),
+  ("Quarter Pounder", false),
+  ("McRib", false),
+  ("Double Double Animal Style", false);
